4cc955d51ccb8add3a579b5a7b2543772ae68631
Bruno Da Silva <99687674+3runoSilva@users.noreply.github.com>
2022-04-28 12:02:15 +0200

    WIP

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-async function getArtist() {
-  try {
-    const response = await axios.get('webdev.fedorageek.org/api/aritst/list');
-    console.log("response",response);
+// async function getArtist() {
+//   try {
+//     const response = await axios.get('webdev.fedorageek.org/api/aritst/list');
+//     console.log("response",response);
 
-    let listArtist = document.getElementById
-  } catch (error) {
-    console.error(error);
-  }
-}
- getArtist()
+//     let listArtist = document.getElementById
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+//  getArtist()
