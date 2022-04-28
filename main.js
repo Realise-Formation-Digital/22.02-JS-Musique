fb108e67756782f8cf2b8a5c0cbe254dcc100904
Bruno Da Silva <99687674+3runoSilva@users.noreply.github.com>
2022-04-28 12:25:10 +0200

    Feat: JS Query (WIP)

--- main.js
+++ main.js
@@ -9,3 +9,22 @@
 //   }
 // }
 //  getArtist()
+
+// ...
+
+// const artist_URL = 'webdev.fedorageek.org/api/aritst/list';
+// const music_URL = 'webdev.fedorageek.org/api/music/list';
+
+// const getTodoItems = async () => {
+//   try {
+//     const response = await axios.get(`${artist_URL}/todos?_limit=5`);
+
+//     const todoItems = response.data;
+
+//     console.log(`GET: Here's the list of todos`, todoItems);
+
+//     return todoItems;
+//   } catch (errors) {
+//     console.error(errors);
+//   }
+// };
