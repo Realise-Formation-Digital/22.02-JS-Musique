d0e80109dd761bda6dd172fa58a22fc0b25215f5
Y0NAT <ezechiel.yonathan.favre@gmail.com>
2022-04-29 16:16:12 +0200

    ko mite

--- index2.js
+++ index2.js
@@ -1,54 +1,4 @@
 let liste = document.getElementById("liste")
-
-// (async () => {
-//   try {
-//      const response = await axios.get('https://api.punkapi.com/v2/beers/1')
-// //   //     // console.log(response.data[0].image_url)
-// //   //     // console.log(response.data[0].name)
-// insertText(response.data[0].name, response.data[0].tagline, response.data[0].image_url)
-// } catch (error) {
-// console.log(error.response.body);
-// }
-//  })();
-
-//   (async () => {
-//       try {
-//         const response = await axios.get('https://api.punkapi.com/v2/beers')
-//       //   console.log(response)
-//         // console.log(response.data[0].name)
-//         //   insertText(response.data[0].name, response.data[0].tagline, response.data[0].image_url)
-//         for (const element of response) {
-//           console.log(data[element].name)}
-
-//       } catch (error) {
-//           console.log(error.response.body);
-//       }
-//     })();
-
-//   function insertText(nameBeer, textBeer, imageBeer){
-//       document.getElementById("name-beer").innerText = nameBeer
-//       document.getElementById("beer-description").innerText = textBeer
-//       document.getElementById("image-beer").innerHTML = imageEl
-//       imageEl.innerHTML =   `<img src=${imageBeer}>
-//       `
-
-//   }
-
-// async function getBeers() {
-//   try {
-//     const response = await axios.get('https://api.punkapi.com/v2/beers')
-//     console.log(response)
-//     // console.log(response.data[0].name)
-//     //   insertText(response.data[0].name, response.data[0].tagline, response.data[0].image_url)
-//     for (const element of response.data) {
-//       console.log(element.name)
-//       liste.innerHTML += "<li>"+element.name+"</li>"
-//     }
-
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
 let nameB = null
 let imageB = null
 let id = null
@@ -65,7 +15,7 @@ async function getMusique() {
       imageB = element.url
 
 
-     liste.innerHTML += `<li>
+     liste.innerHTML += ` <div class="container"><li class="container-fluid">
   
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target=#`+ nameB + `>
   `+ nameB + `
@@ -80,8 +30,8 @@ async function getMusique() {
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-      <`+ id + `>
-      ` + imageB + `
+      <`+id+ `>
+      `+imageB + `
       <div class="plyr__video-embed container" id="player">
   <iframe class="container-fluid"
     src="https://www.youtube.com/embed/H3DGpINHX5Q"
@@ -99,12 +49,11 @@ async function getMusique() {
     </div>
   </div>
 </div>
-      </li>`
+      </li></div>`
     }
 
-  } catch (error) {
+  }catch (error){
     console.log(error);
   }
 }
-
 await getMusique()
