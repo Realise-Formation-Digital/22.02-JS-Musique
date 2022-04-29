9df32ccfda54331e086f3391d055b0b5e787d445
Bruno Da Silva <99687674+3runoSilva@users.noreply.github.com>
2022-04-29 14:14:42 +0200

    wip

--- main.js
+++ main.js
@@ -1,30 +1,85 @@
-// async function getArtist() {
-//   try {
-//     const response = await axios.get('webdev.fedorageek.org/api/aritst/list');
-//     console.log("response",response);
+let liste = document.getElementById("liste")
 
-//     let listArtist = document.getElementById
+// (async () => {
+//   try {
+//      const response = await axios.get('https://api.punkapi.com/v2/beers/1')
+// //   //     // console.log(response.data[0].image_url)
+// //   //     // console.log(response.data[0].name)
+// insertText(response.data[0].name, response.data[0].tagline, response.data[0].image_url)
+// } catch (error) {
+// console.log(error.response.body);
+// }
+//  })();
+
+//   (async () => {
+//       try {
+//         const response = await axios.get('https://api.punkapi.com/v2/beers')
+//       //   console.log(response)
+//         // console.log(response.data[0].name)
+//         //   insertText(response.data[0].name, response.data[0].tagline, response.data[0].image_url)
+//         for (const element of response) {
+//           console.log(data[element].name)}
+
+//       } catch (error) {
+//           console.log(error.response.body);
+//       }
+//     })();
+
+//   function insertText(nameBeer, textBeer, imageBeer){
+//       document.getElementById("name-beer").innerText = nameBeer
+//       document.getElementById("beer-description").innerText = textBeer
+//       document.getElementById("image-beer").innerHTML = imageEl
+//       imageEl.innerHTML =   `<img src=${imageBeer}>
+//       `
+
+//   }
+
+// async function getBeers() {
+//   try {
+//     const response = await axios.get('https://api.punkapi.com/v2/beers')
+//     console.log(response)
+//     // console.log(response.data[0].name)
+//     //   insertText(response.data[0].name, response.data[0].tagline, response.data[0].image_url)
+//     for (const element of response.data) {
+//       console.log(element.name)
+//       liste.innerHTML += "<li>"+element.name+"</li>"
+//     }
+
 //   } catch (error) {
-//     console.error(error);
+//     console.log(error);
 //   }
 // }
-//  getArtist()
+let nameB = null
+let imageB = null
+let id = null
+async function getArtiste() {
+  try {
+    const response = await axios.get('https://webdev.fedorageek.org/api/artistes/list')
+    console.log(response.data)
+    // console.log(response.data[0].name)
+    //   insertText(response.data[0].name, response.data[0].tagline, response.data[0].image_url)
+    for (const element of response.data) {
 
-// ...
+      nameB = element.nom
+      id = element.id
+      
 
-// const artist_URL = 'webdev.fedorageek.org/api/aritst/list';
-// const music_URL = 'webdev.fedorageek.org/api/music/list';
+    
+     liste.innerHTML += ` <div>
+     <div class="btn">
+       <div class="number"><i class='fas fa-shield-alt' style='font-size:24px'></i></div>
+       <div class="content">
+         <link data-fancybox data-src="#" href="">
+         <h2>Nom de l'artiste</h2>
+         </link>
+       </div>
+     </div>
+   </div>`
+    }
 
-// const getTodoItems = async () => {
-//   try {
-//     const response = await axios.get(`${artist_URL}/todos?_limit=5`);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-//     const todoItems = response.data;
-
-//     console.log(`GET: Here's the list of todos`, todoItems);
-
-//     return todoItems;
-//   } catch (errors) {
-//     console.error(errors);
-//   }
-// };
+await getMusique()
