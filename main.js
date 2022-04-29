77dd4566d8da47c32e15f2a0b569e6707fb66469
Y0NAT <ezechiel.yonathan.favre@gmail.com>
2022-04-29 09:44:02 +0200

    .

--- main.js
+++ main.js
@@ -5,21 +5,42 @@ async function getUser() {
       const response = await axios.get('https://webdev.fedorageek.org/api/musique/list');
       //console.log(response);
 
-        let listDiv = document.getElementById('listBeer')
+        let listDiv = document.getElementById('listMusic')
 
-      for (let beer of response.data){
-          console.log(beer)
+      for (let music of response.data){
+          console.log(music)
 
             // crée un nouvel élément div
             var newDiv = document.createElement("div");
             // et lui donne un peu de contenu
-            var newContent = document.createTextNode(beer.nom);
+            var newContent = document.createTextNode(music.nom);
             // ajoute le nœud texte au nouveau div créé
             newDiv.appendChild(newContent);
 
             // ajoute le nouvel élément créé et son contenu dans le DOM
             document.body.insertBefore(newDiv, listDiv);
 
+
+            // crée un nouvel élément div
+            var newDiv = document.createElement("div");
+            // et lui donne un peu de contenu
+            var newContent = document.createTextNode(music.album);
+            // ajoute le nœud texte au nouveau div créé
+            newDiv.appendChild(newContent);
+
+            // ajoute le nouvel élément créé et son contenu dans le DOM
+            document.body.insertBefore(newDiv, listDiv);
+
+
+            // // crée un nouvel élément div
+            // var newDiv = document.createElement("div");
+            // // et lui donne un peu de contenu
+            // var newContent = document.createTextNode(music.url);
+            // // ajoute le nœud texte au nouveau div créé
+            // newDiv.appendChild(newContent);
+
+            // // ajoute le nouvel élément créé et son contenu dans le DOM
+            // document.body.insertBefore(newDiv, listDiv);
 
       }
 
