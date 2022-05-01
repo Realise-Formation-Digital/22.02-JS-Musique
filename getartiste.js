5436791fca055cf1f03fe836830984e09cf16dea
Bruno Da Silva <99687674+3runoSilva@users.noreply.github.com>
2022-05-01 22:15:01 +0200

    Fix: data display music
    Fixed the display of data from the DB  on both artiste and music + added btn for the modal url display.
    (WIP):still trying to link both artist and music + tried to add style of music to see how it differs from the other two but so far didn't managed to make it work.

--- getartiste.js
+++ getartiste.js
@@ -1,82 +1,87 @@
-let divArtiste = document.getElementById("listArtiste");
-let divMusique = document.createElement("div");
+let divArtiste = document.getElementById("li");
+let divMusique = document.getElementById("card")
 
 
-let artiste = null;
-let musique = null;
-let id = null;
 
-async function getArtiste() {
+
+async function getData() {
   try {
-    const artisteList = await axios.get('https://webdev.fedorageek.org/api/artistes/list')
-    const musiqueList = await axios.get('https://webdev.fedorageek.org/api/musique/list')
-    //  console.log(response.data)
-     for (const listArt of artisteList.data)  {
-     console.log(artisteList.data);
 
-       artiste = listArt.nom
-       id = listArt.id
+    /**
+     * Requete (GET) pour chercher les element sur l'API  
+     */
+     
+    const artisteList = await axios.get('https://webdev.fedorageek.org/api/artistes/list');
+    const musiqueList = await axios.get('https://webdev.fedorageek.org/api/musique/list');
+    const styleList = await axios.get('https://webdev.fedorageek.org/api/style/list')
       
+
+    /**
+     * Boucle FOR OF pour parcourir l'ARRAY artisteList.data et afficher les ARTISTE 
+     * 
+     * @data  [id, nom, groupe]
+     * 
+     */
+     for (const artist of artisteList.data)  {
+     console.log(artisteList.data);
 
     
        divArtiste.innerHTML += `
         <li class="item">
-          <input type="radio" id="`+id+`" name="basic_carousel" value="first" checked="checked"/>
-          <label class="label_one" for="one">`+artiste+`</label>
-          <div class="content content_one">
-            <span class="picto"></span>
-            <p>fourth</p>
+          <input type="radio" id="one" name="basic_carousel" value="first" checked="checked"/>
+          <label class="label_${artist.id}" for="one">${artist.nom}</label>
+        </li>`  
+    }
 
-            <div class="container">
+   
+
+    /**
+     * Boucle FOR OF pour parcourir l'ARRAY musiqueList.data et afficher les MUSIQUE
+     * 
+     * @data[id, nom, url]
+     * 
+     */
+    for (const music of musiqueList.data)  {
+    console.log(musiqueList.data);
+      
+    
+    divMusique.innerHTML += `
+            
               <div class="card">
-                <h3 class="title">Card 1</h3>
+               <h3 class="title">${music.nom}</h3>
                 <div class="bar">
                   <div class="emptybar"></div>
                   <div class="filledbar"></div>
-                </div>
-              </div>
-          </div>
-        </li>   
-  `
+                  <button type="button" class="btn btn-light">Watch ${music.nom}</button>
+                  <p id"style"></p>
+                </div>`
     }
 
-    for (const listMus of musiqueList.data) {
-      console.log(musiqueList.data);
-
-       musique = listMus.nom
-       id = listMus.id
-      
 
     
-       divMusique.innerHTML += `
-       <div class="content content_one">
-       <span class="picto"></span>
-       <p></p>
+    /**
+     * Boucle FOR OF pour parcourir l'ARRAY styleList.data et afficher les STYLE
+     * 
+     *  @data [id, nom, type]
+     * 
+     */
+  //   for (const style of styleList.data)  {
+  //    console.log(styleList.data);
+      
+    
+  //    divMusique.innerHTML += `<p>hello</p>`
+            
+             
+  //  }
+       
 
-       <div class="container">
-         <div class="card">
-           <h3 class="title">Card 1</h3>
-           <div class="bar">
-             <div class="emptybar"></div>
-             <div class="filledbar"></div>
-           </div>
-         </div>
-         <div class="card">
-           <h3 class="title">Card 1</h3>
-           <div class="bar">
-             <div class="emptybar"></div>
-             <div class="filledbar"></div>
-           </div>
-         </div>
-       </div>
-     </div>  
-  `
-    }
+
+
 
   } catch (error) {
     console.log(error);
   }
 }
 
-await getArtiste();
+await getData();
 
